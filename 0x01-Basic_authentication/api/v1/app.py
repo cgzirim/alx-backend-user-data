4c5d775b7dd2c1886c3024a4d9be9bd1c3e27f15
Chigozirim <iamokwukwe@gmail.com>
2022-07-26 16:27:28 +0100

    Update forbidden error handler

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -54,7 +54,7 @@ def unauthorized(error) -> str:
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """Forbidden handler"""
-    return jsonify({"error": "Forbidden"})
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
