bccdca7c4425c3dc552b1dc1e092f460fa4c5535
Chigozirim <iamokwukwe@gmail.com>
2022-07-28 22:20:30 +0100

    Store user session

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """ User Session module
 """
-import hashlib
 from models.base import Base
 
 
@@ -10,5 +9,6 @@ class UserSession(Base):
 
     def __init__(self, *args: list, **kwargs: dict):
         """Initialize session instance."""
+        super().__init__(*args, **kwargs)
         self.user_id = kwargs.get("user_id")
         self.session_id = kwargs.get("session_id")
