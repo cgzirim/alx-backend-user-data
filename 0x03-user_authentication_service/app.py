8d795f24442455f1719162812e5d002bc3e5f92c
Chigozirim <iamokwukwe@gmail.com>
2022-08-02 21:59:26 +0100

    Create /sessions route and login method to handle it

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -11,13 +11,17 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello() -> str:
-    """Returns JSON payload."""
+    """GET /
+    Returns JSON payload.
+    """
     return jsonify({'message': 'Bienvenue'})
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def users():
-    """Registers a user and returns a JSON payload."""
+    """POST /users
+    Registers a user and returns a JSON payload.
+    """
     email = request.form.get('email')
     password = request.form.get('password')
 
@@ -31,6 +35,31 @@ def users():
             "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """POST /sessions
+    Validates email and password in POST request.
+    Return:
+        - Error code 401 if email doesn't exist or the password isn't valid
+        - JSON payload with a cookie attached to the response.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or password is None:
+        abort(401)
+
+    if AUTH.valid_login(email, password) is False:
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+
+    # Set cookie to the response
+    response = jsonify({"email": "{}".format(email), "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+
+    return response
 
 
 if __name__ == "__main__":
