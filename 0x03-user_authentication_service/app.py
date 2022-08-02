641e8a55718b5a195dccde7b9119f16e0d8c44c3
Chigozirim <iamokwukwe@gmail.com>
2022-08-02 15:58:15 +0100

    Create route to register users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """Basic Flask app."""
+from auth import Auth
 from flask import Flask, jsonify
+from flask import request, abort
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -12,5 +15,23 @@ def hello() -> str:
     return jsonify({'message': 'Bienvenue'})
 
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """Registers a user and returns a JSON payload."""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email or not password:
+        abort(400)
+
+    try:
+        AUTH.register_user(email=email, password=password)
+        return jsonify({
+            "email": "{}".format(email),
+            "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000", debug=True)
