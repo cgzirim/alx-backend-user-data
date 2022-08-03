38bd0be4237603dc0fd63ccb5a3838b6775708bb
Chigozirim <iamokwukwe@gmail.com>
2022-08-03 14:59:09 +0100

    Make update_user method typed

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -57,7 +57,7 @@ class DB:
 
         return user
 
-    def update_user(self, user_id, **kwargs) -> None:
+    def update_user(self, user_id: int, **kwargs) -> None:
         """Updates a User instance."""
         user = self.find_user_by(id=user_id)
 
