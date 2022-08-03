ace7b674d28fc4ff07f0c2fb1c1f7238d7d22a15
Chigozirim <iamokwukwe@gmail.com>
2022-08-03 12:57:04 +0100

    Correct redirect path

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Basic Flask app."""
 from auth import Auth
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect
 from flask import request, abort
 
 
@@ -9,21 +9,21 @@ AUTH = Auth()
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
+@app.route("/", methods=["GET"], strict_slashes=False)
 def hello() -> str:
     """GET /
     Returns JSON payload.
     """
-    return jsonify({'message': 'Bienvenue'})
+    return jsonify({"message": "Bienvenue"})
 
 
-@app.route('/users', methods=['POST'], strict_slashes=False)
+@app.route("/users", methods=["POST"], strict_slashes=False)
 def users():
     """POST /users
     Registers a user and returns a JSON payload.
     """
-    email = request.form.get('email')
-    password = request.form.get('password')
+    email = request.form.get("email")
+    password = request.form.get("password")
 
     if not email or not password:
         abort(400)
@@ -37,16 +37,16 @@ def users():
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route('/sessions', methods=['POST'], strict_slashes=False)
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
 def login():
     """POST /sessions
     Validates email and password in POST request.
-    Return:
-        - Error code 401 if email doesn't exist or the password isn't valid
+    Respond:
+        - 401 HTTP status if email doesn't exist or the password isn't valid
         - JSON payload with a cookie attached to the response.
     """
-    email = request.form.get('email')
-    password = request.form.get('password')
+    email = request.form.get("email")
+    password = request.form.get("password")
     if email is None or password is None:
         abort(401)
 
@@ -60,6 +60,24 @@ def login():
     response.set_cookie("session_id", session_id)
 
     return response
+
+
+@app.route("/sessions", methods=["DELETE"], strict_slashes=True)
+def logout():
+    """DELETE /session
+    If the user exists, destroys the session and redirect the user
+    to GET /. Otherwise, respond with a 403 HTTP status
+    """
+    session_id = request.cookies.get("session_id")
+    if session_id is None:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+
+    AUTH.destroy_session(user.id)
+    return redirect("/", 302)
 
 
 if __name__ == "__main__":
